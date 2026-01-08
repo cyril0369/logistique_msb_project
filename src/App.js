738d69be90b8a0fe4ba86e822229980d723ec101
cyril <cyril@haberer.fr>
2026-01-08 15:26:48 +0100

    add the page inscription participant

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import AcceuilConnexion from "./pages/AcceuilConnexion";
 import AcceuilInscriprion from "./pages/AcceuilInscriprion";
 import AcceuilInscriprionTeamMSB from "./pages/AcceuilInscriprionTeamMSB.jsx";
 import AcceuilInscriprionStaff from './pages/AcceuilInscriprionStaff.jsx';
+import AcceuilInscriprionParticipant from './pages/AcceuilInscriprionParticipant.jsx';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path="/acceuil/inscription" element={<AcceuilInscriprion />} />
           <Route path="/acceuil/inscription/team" element={<AcceuilInscriprionTeamMSB />} />
           <Route path="/acceuil/inscription/staff" element={<AcceuilInscriprionStaff />} />
+          <Route path="/acceuil/inscription/participant" element={<AcceuilInscriprionParticipant />} />
         </Routes>
       </div>
     </Router>
---
+++ src/components/FormulaireInscriptionParticipant.jsx
@@ -0,0 +1,96 @@
+import { useNavigate } from 'react-router-dom';
+
+export default function FormulaireInscriptionParticipant() {
+    const navigate = useNavigate();
+
+    return(
+        <div className="Formulaire">
+            <div className="titre-sous-titre block">
+                <h1>Inscription<br />Participant</h1>
+            </div>
+            <div className="champ-de-saisie block">
+                <h4>Informations Personnelles</h4>
+                <p className="corps-2">Taille de t-shirt *</p>
+                <div className='cases-a-cocher'>
+                    <div className="case-a-cocher">
+                        <input type="checkbox" className='cases'/>
+                        <p className="corps-2">S</p>
+                    </div>
+                    <div className="case-a-cocher">
+                        <input type="checkbox" className='cases'/>
+                        <p className="corps-2">M</p>
+                    </div>
+                    <div className="case-a-cocher">
+                        <input type="checkbox" className='cases'/>
+                        <p className="corps-2">L</p>
+                    </div>
+                </div>  
+                <p className="corps-2">Régime alimentaire *</p>
+                <div className='cases-a-cocher'>
+                    <div className="case-a-cocher">
+                        <input type="checkbox" className='cases'/>
+                        <p className="corps-2">Végétarien</p>
+                    </div>
+                    <div className="case-a-cocher">
+                        <input type="checkbox" className='cases'/>
+                        <p className="corps-2">Halal</p>
+                    </div>
+                    <div className="case-a-cocher">
+                        <input type="checkbox" className='cases'/>
+                        <p className="corps-2">Allergies</p>
+                    </div>
+                </div>  
+                <p className="corps-2">Remarques / spécifications</p>
+                <input type="text" className='input-text'/>
+                <p className="corps-2">Ecole *</p>
+                <input type="text" className='input-text'/>
+            </div>
+
+            <div className="champ-de-saisie block">
+                <h4>Information tournoi</h4>
+                <p className="corps-2">Sport *</p>
+                <input type="text" className='input-text'/>
+                <p className="corps-2">Type de tournoi *</p>
+                <div className='cases-a-cocher'>
+                    <div className="case-a-cocher">
+                        <input type="checkbox" className='cases'/>
+                        <p className="corps-2">Championship</p>
+                    </div>
+                    <div className="case-a-cocher">
+                        <input type="checkbox" className='cases'/>
+                        <p className="corps-2">Loisir</p>
+                    </div>
+                </div> 
+                <p className="corps-2">Composition *</p>
+                <div className='cases-a-cocher'>
+                    <div className="case-a-cocher">
+                        <input type="checkbox" className='cases'/>
+                        <p className="corps-2">Femme</p>
+                    </div>
+                    <div className="case-a-cocher">
+                        <input type="checkbox" className='cases'/>
+                        <p className="corps-2">Homme</p>
+                    </div>
+                    <div className="case-a-cocher">
+                        <input type="checkbox" className='cases'/>
+                        <p className="corps-2">Mixte</p>
+                    </div>
+                </div> 
+                <p className="corps-2">Nombre de joueurs *</p>
+                <input type="text" className='input-text'/>
+                <p className="corps-2">Nom d’équipe *</p>
+                <input type="text" className='input-text'/>
+                <p className="corps-2">Nom du capitaine d’équipe *</p>
+                <input type="text" className='input-text'/>
+            </div>
+
+            <div className="boutton-mdp-oublier block">
+                <button className="se-connecter">
+                    <h4>S’inscrire</h4>
+                </button>
+                <p className="corps-2">* Champs obligatoire</p>
+                
+            </div>
+        </div>
+    )
+}
