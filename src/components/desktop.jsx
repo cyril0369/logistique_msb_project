d5fedde2951891f5b546959c8aee9cb60109d00c
cyril <cyril@haberer.fr>
2026-02-09 14:04:18 +0100

    correction du boutton burger

--- src/components/desktop.jsx
+++ src/components/desktop.jsx
@@ -1,29 +1,48 @@
 import { useState } from "react";
-import { GiHamburgerMenu } from "react-icons/gi";
 import { IoPersonCircleOutline } from "react-icons/io5";
-import LogoCouleur from "../images/464515b70379eaca6faa8af8ddfae9b805a7bb96.png"
+import LogoCouleur from "../images/464515b70379eaca6faa8af8ddfae9b805a7bb96.png";
 import Sidebare from "./Sidebare.jsx";
 
 export default function Desktop() {
+  const [isSidebarHidden, setIsSidebarHidden] = useState(true);
 
-    const [isSidebarHidden, setIsSidebarHidden] = useState(false);
-    
-    const toggleSidebar = () => {
-        setIsSidebarHidden(!isSidebarHidden);
-    };
-    
-    return(
-        <>
-        <header className="desktop">
-            <GiHamburgerMenu className="hamburger-menu" onClick={toggleSidebar} />
+  function toggleHamburger() {
+    setIsSidebarHidden(prev => !prev);
+  }
+
+  const isOpen = !isSidebarHidden;
+
+  return (
+    <>
+      <header className="desktop">
+        <button
+          className={`hamburger-btn ${isOpen ? "open" : ""}`}
+          aria-pressed={isOpen}
+          aria-label="Menu"
+          onClick={toggleHamburger}
+        >
+          {isOpen ? (
             <img
-                className="LogoDesktop"
-                src={LogoCouleur}
-                alt="LOGO DESKTOP"
-                />
-            <IoPersonCircleOutline className="icon-connextion" />
-        </header>
-        <Sidebare isHidden={isSidebarHidden} />
-        </>
-    )
+              className="hamburger-icon icon-close"
+              src="/assets/icon-close.svg"
+              alt=""
+              aria-hidden="false"
+            />
+          ) : (
+            <img
+              className="hamburger-icon icon-menu"
+              src="/assets/icon-menu.svg"
+              alt=""
+              aria-hidden="false"
+            />
+          )}
+        </button>
+
+        <img className="LogoDesktop" src={LogoCouleur} alt="LOGO DESKTOP" />
+        <IoPersonCircleOutline className="icon-connextion" />
+      </header>
+
+      <Sidebare isHidden={isSidebarHidden} />
+    </>
+  );
 }
