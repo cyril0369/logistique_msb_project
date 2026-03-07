e7416a4032b57cb0dc125a511a6a94d9cb8c8dc3
cyril <cyril@haberer.fr>
2026-03-07 17:35:03 +0100

    import

--- src/components/FormulaireInscriptionStaff.jsx
+++ src/components/FormulaireInscriptionStaff.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
+import {authService} from '../services/authService';
 
 export default function FormulaireInscriptionStaff() {
     const navigate = useNavigate();
