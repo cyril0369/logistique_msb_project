676fae52eec2ffa68b224faf08cb328000b33b0f
cyril <cyril@haberer.fr>
2026-03-10 13:38:44 +0100

    corrige lurl de l'api

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000",
-  // ou "http://localhost:5000" selon ton backend
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000",
 });
 
 // ✅ Ajoute le token automatiquement
